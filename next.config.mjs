a59c311ddcf76d17898b8d41f346453f2ae8ea32
eschoepf <eduardo.schoepf@hotmail.com>
2025-08-11 16:14:04 -0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/portfolio' : '',
+  assetPrefix: isProd ? '/portfolio/' : '',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
